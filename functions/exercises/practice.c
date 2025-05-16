a7a6e306416ef0d629de1582cac5e96344b5a8f9
Fasina Ayo <skyjarmer@gmail.com>
2025-05-16 12:49:26 +0100

    practice for function exercises

---
+++ functions/exercises/practice.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+
+
+double triangle_area( double base, double height)
+
+// take input for triangle base and height in a main and print
+double main (void)
+{
+    printf("enter digits/");
+    scanf("%d %d", = (triangle_area(double, double)) );
+
+    return 0;
+}
+// function for computation of the area
+{
+    double product;
+    product = base * height;
+    return product / 2;
+}
